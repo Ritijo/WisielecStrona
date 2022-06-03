8bf7d992b43e399fefb04a3200afd821ae9e2b66
MateodiStefano <56407016+MateodiStefano@users.noreply.github.com>
2022-06-03 05:19:17 +0200

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -9,16 +9,8 @@ xhr.onload = function() {
 }
 xhr.send();
 
-var bar = new XMLHttpRequest ();
-bar.open("POST", "getword.php");
-bar.onload = function() {
-    var jsvar = this.response;
-    document.getElementById("translation").innerHTML = (jsvar);
-}
-bar.send();
-
 var password = xhr;
-var translation = bar;
+var translation = xhr;
 
 var mistake_count = 0;
 
